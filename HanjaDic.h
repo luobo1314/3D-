33d222e0e6356e37c832ee714245222229b110b6
luobo1314 <yxbingbing@gmail.com>
2025-05-23 17:47:29 +0800

    Add files via upload

---
+++ HanjaDic.h
@@ -0,0 +1,12 @@
+< head >
+< script 
+ src ="https://aframe.io/releases/1.2.O/aframe. min . js "></ script >
+</ head >
+< body >
+< a - scene >
+< a - entity geometry =" primitive : box "
+ material =" color :#4CC3D9" position =" O 1.5-3"></ a - entity >
+< a - sky color ="# ECECEC "></ a - sky >
+</ a - scene >
+</ body >
+</ html >
